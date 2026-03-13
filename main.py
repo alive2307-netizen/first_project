f814ceeca86d5e5310f9b846051a568315e6eb11
Alex Ivaschenko <alive2307@gmail.com>
2026-03-13 16:19:15 +0300

    Add dotenv functionality and ignore .env file

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 print('Hello from repository!') 
+
+import os
+from dotenv import load_dotenv
+
+def print_author():
+    # Загружаем переменные из .env файла
+    load_dotenv()
+    
+    # Читаем значение переменной AUTHOR
+    author = os.getenv('AUTHOR')
+    
+    # Выводим имя автора
+    print(f"Автор проекта: {author}")
+
+# Вызов функции для проверки
+if __name__ == "__main__":
+    print_author()
